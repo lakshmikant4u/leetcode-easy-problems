9ab09ee5cca41f39b1053dd8f4c111093f07bcc6
LAKSHMIKANT <lakshmikant4u@gmail.com>
2022-12-04 21:50:32 +0530

    Array : Javascript solution to find the single number

---
+++ array/js/single-number.js
@@ -0,0 +1,43 @@
+/** https://leetcode.com/problems/single-number/
+ 
+Single Number
+
+Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+Test Case 1
+Input: nums = [2,2,1]
+Output: 1
+
+Test case 2
+Input: nums = [4,1,2,1,2]
+Output: 4
+
+Test Case 3
+Input: nums = [1]
+Output: 1
+
+Constraints:
+
+1 <= nums.length <= 3 * 104
+-3 * 104 <= nums[i] <= 3 * 104
+Each element in the array appears twice except for one element which appears only once.
+
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+// Solution 1 using normal for loop to sort and compare consecutive pairs
+const singleNumber = (nums) => {
+    nums.sort((a, b) => a - b);
+    for (let i = 0; i < nums.length - 1; i += 2) {
+        if (nums[i] != nums[i + 1]) return nums[i]
+    }
+    return nums[nums.length - 1];
+};
+
+console.log(singleNumber([2, 2, 1])); // 1
