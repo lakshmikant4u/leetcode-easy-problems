ed3de3618d1d0faae7715c80887c3dba240a8d57
LAKSHMIKANT <lakshmikant4u@gmail.com>
2022-12-28 11:50:07 +0530

    Javascript solution for Palindrome

---
+++ array/js/palindrome-number.js
@@ -0,0 +1,43 @@
+/** https://leetcode.com/problems/palindrome-number/
+
+Palindrome Number
+
+Given an integer x, return true if x is a palindrome, and false otherwise.
+
+Test Case 1
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+
+Test Case 2
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+
+Test Case 3
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+
+ */
+
+
+// Solution 1
+/**
+ * 
+ * @param {*} x 
+ * @returns 
+ */
+const isPalindrome = function (x) {
+    if (x < 0) return false;
+    let reverse = 0;
+    let temp = x;
+    while (temp != 0) {
+        reverse = reverse * 10 + temp % 10;
+        temp = parseInt(temp / 10);
+    }
+    return x === reverse;
+};
+
+console.log(isPalindrome(121)); // true
+
