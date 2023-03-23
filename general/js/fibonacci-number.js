4edd660241afe76327046fb2516162df21df6a71
LAKSHMIKANT <lakshmikant4u@gmail.com>
2023-03-24 00:03:17 +0530

    JS solution for Fibonacci Number one liner

---
+++ general/js/fibonacci-number.js
@@ -0,0 +1,36 @@
+/** https://leetcode.com/problems/fibonacci-number/
+
+Fibonacci Number
+
+The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence,
+such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+
+F(0) = 0, F(1) = 1
+F(n) = F(n - 1) + F(n - 2), for n > 1.
+Given n, calculate F(n).
+
+Test Case 1
+Input: n = 2
+Output: 1
+Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+
+Test Case 2
+Input: n = 3
+Output: 2
+Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
+
+Test Case 3
+Input: n = 4
+Output: 3
+Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
+
+Constraints:
+
+0 <= n <= 30
+ */
+
+// Solution 1 - Dynamic Programming
+
+const fib = (n, res = [0, 1]) => (n == 0 || n == 1 || res[n]) ? res[n] : fib(n - 1, res) + fib(n - 2, res);
+
+console.log(fib(4)); // 3
